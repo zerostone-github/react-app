ab0b1c67a94b04ab2d994ae21d9493d9880f3530
youngseock kim <zerostone@msn.com>
2021-09-26 10:31:19 +0900

    추가 코드

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
+
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">JavaScript</a></li>
+          </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+          <h2>HTML</h2>
+          HTML is HyperText Markup Lanaguage.
+      </article>
+    );
+  }
+}
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>
+    );
+  }
+}
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Hello, react !!
+        <Subject title="WEB" sub="World Wide Web!!"></Subject>
+        <Subject title="REACT" sub="this is amazing!!"></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
-
-
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
